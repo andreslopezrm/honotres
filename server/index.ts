d56a345f148709e35079076362a5c8a4ca2073b3
Andres Lopez <andres_lopez_rm@hotmail.com>
2024-07-31 11:51:26 -0600

    Add files via upload

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import { serve } from "@hono/node-server";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { createRouteList } from "../api/routes";
+
+const app = new OpenAPIHono().basePath("/api");
+
+const port = 3000;
+createRouteList(app);
+
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
