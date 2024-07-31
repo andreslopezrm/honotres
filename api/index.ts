137aeba4957be02d00872aabfe002ddcd148f536
Andres Lopez <andres_lopez_rm@hotmail.com>
2024-07-31 12:06:02 -0600

    Add files via upload

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,51 @@
-import { OpenAPIHono } from "@hono/zod-openapi";
+import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
+import { swaggerUI } from "@hono/swagger-ui";
 import { handle } from "hono/vercel";
-import { createRouteList } from "../server/routes";
+
+const createRouteList = (app: OpenAPIHono) => {
+    app.openapi(
+      createRoute({
+        method: "get",
+        path: "/",
+        responses: {
+          200: {
+            description: "Respond a message",
+            content: {
+              "application/json": {
+                schema: z.object({
+                  message: z.string(),
+                }),
+              },
+            },
+          },
+        },
+      }),
+      (c) => {
+        return c.json({
+          message: "hello from vercel",
+        });
+      },
+    );
+  
+    app.get(
+      "/ui",
+      swaggerUI({
+        url: "/api/doc",
+      }),
+    );
+  
+    app.doc("/doc", {
+      info: {
+        title: "An API",
+        version: "v1",
+      },
+      openapi: "3.1.0",
+    });
+  };
 
 
 const app = new OpenAPIHono().basePath("/api");
-
-createRouteList(app);
+createRouteList(app)
 const handler = handle(app);
 
 export const GET = handler;
