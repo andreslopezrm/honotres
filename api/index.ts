e06ba046c4f855f84b3459c4cc19a67ae956a211
Andres Lopez <andres_lopez_rm@hotmail.com>
2024-07-30 18:34:28 -0600

    Add files via upload

--- api/index.ts
+++ api/index.ts
@@ -1,50 +1,23 @@
-import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
+import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { swaggerUI } from "@hono/swagger-ui";
 
+/*
 export const config = {
   runtime: "edge",
 };
+*/
 
-const app = new OpenAPIHono().basePath("/api");
+const app = new Hono().basePath("/api");
 
-app.openapi(
-  createRoute({
-    method: "get",
-    path: "/hello",
-    responses: {
-      200: {
-        description: "Respond a message",
-        content: {
-          "application/json": {
-            schema: z.object({
-              message: z.string(),
-            }),
-          },
-        },
-      },
-    },
-  }),
-  (c) => {
-    return c.json({
-      message: "hello",
-    });
-  },
-);
-
-app.get(
-  "/ui",
-  swaggerUI({
-    url: "/doc",
-  }),
-);
-
-app.doc("/doc", {
-  info: {
-    title: "An API",
-    version: "v1",
-  },
-  openapi: "3.1.0",
+app.get("/", (c) => {
+  return c.json({ message: "Hello Hono!" });
 });
+
+app.get("/demo", (c) => {
+  return c.json({ message: "Demos" });
+});
+
+app.get("/ui", swaggerUI({ url: "/doc" }));
 
 export default handle(app);
