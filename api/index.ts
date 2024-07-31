d5b20091141e237d9de57045d73fe69363fa3000
Andres Lopez <andres_lopez_rm@hotmail.com>
2024-07-31 11:45:35 -0600

    Add files via upload

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { handle } from "hono/vercel";
+import { createRouteList } from "../lib/routes";
+
+const app = new OpenAPIHono().basePath("/api");
+
+createRouteList(app);
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
