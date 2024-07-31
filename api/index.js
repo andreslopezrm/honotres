8319843412c1a6d9ede821a774315685c33ccc18
Andres Lopez <andres_lopez_rm@hotmail.com>
2024-07-30 18:51:13 -0600

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,11 +1,46 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
+import { handle } from "hono/vercel";
 
-const app = new Hono().basePath('/api')
+const app = new OpenAPIHono().basePath("/api");
 
-app.get('/', (c) => {
-  return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
-})
+app.openapi(
+  createRoute({
+    method: "get",
+    path: "/",
+    responses: {
+      200: {
+        description: "Respond a message",
+        content: {
+          "application/json": {
+            schema: z.object({
+              message: z.string(),
+            }),
+          },
+        },
+      },
+    },
+  }),
+  (c) => {
+    return c.json({
+      message: "hello",
+    });
+  },
+);
+
+app.get(
+  "/ui",
+  swaggerUI({
+    url: "/doc",
+  }),
+);
+
+app.doc("/doc", {
+  info: {
+    title: "An API",
+    version: "v1",
+  },
+  openapi: "3.1.0",
+});
 
 const handler = handle(app);
 
