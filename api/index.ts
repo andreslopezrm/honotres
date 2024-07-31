c8d31c1c60acbc96bc6440fb296a4b2da765cef9
Andres Lopez <andres_lopez_rm@hotmail.com>
2024-07-30 18:23:48 -0600

    Add files via upload

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,21 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { swaggerUI } from "@hono/swagger-ui";
 
 export const config = {
-  runtime: 'edge'
-}
+  runtime: "edge",
+};
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
-})
+app.get("/", (c) => {
+  return c.json({ message: "Hello Hono!" });
+});
 
-export default handle(app)
+app.get("/demo", (c) => {
+  return c.json({ message: "Demos" });
+});
+
+app.get("/ui", swaggerUI({ url: "/doc" }));
+
+export default handle(app);
